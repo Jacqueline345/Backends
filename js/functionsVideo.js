0db005da0f276939d878ca8fcc23e1ba57c944ef
abenavides01 <andrepaobena@gmail.com>
2025-03-25 12:07:02 -0600

    Búsqueda de videos lista

--- js/functionsVideo.js
+++ js/functionsVideo.js
@@ -132,3 +132,15 @@ async function deleteVideo(videoId) {
         console.error('Error al eliminar el video:', error);
     }
 }
+
+// Función para convertir URL de YouTube a formato embed
+function convertToEmbedUrl(url) {
+    const videoId = url.split('v=')[1];
+    const ampersandPosition = videoId.indexOf('&');
+    if (ampersandPosition !== -1) {
+        return `https://www.youtube.com/embed/${videoId.substring(0, ampersandPosition)}`;
+    }
+    return `https://www.youtube.com/embed/${videoId}`;
+}
+
+module.exports = { convertToEmbedUrl };
