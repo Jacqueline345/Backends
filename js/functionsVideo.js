fb39cd4f485519b9e8e86b1cb966ba0b9e29b18d
abenavides01 <andrepaobena@gmail.com>
2025-03-25 12:43:49 -0600

    Corrigiendo error

--- js/functionsVideo.js
+++ js/functionsVideo.js
@@ -133,14 +133,3 @@ async function deleteVideo(videoId) {
     }
 }
 
-// Función para convertir URL de YouTube a formato embed
-function convertToEmbedUrl(url) {
-    const videoId = url.split('v=')[1];
-    const ampersandPosition = videoId.indexOf('&');
-    if (ampersandPosition !== -1) {
-        return `https://www.youtube.com/embed/${videoId.substring(0, ampersandPosition)}`;
-    }
-    return `https://www.youtube.com/embed/${videoId}`;
-}
-
-module.exports = { convertToEmbedUrl };
